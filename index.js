7c9e1c299998c335f58766d352728e247aa1b204
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-19 23:54:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes ={}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object
+  var cloneObject = object[key] = value;
+  return cloneObject
 }
