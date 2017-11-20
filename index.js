9765990f8c7f3a1cc35cb894c7289f517542506a
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-20 00:08:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key, value){
   object[key] =value;
   return object
 }
-function deleteFromObjectByKey(object, key, value){
-
+function deleteFromObjectByKey(object, key){
+return Object.assign({}, object, delete object[key])
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
